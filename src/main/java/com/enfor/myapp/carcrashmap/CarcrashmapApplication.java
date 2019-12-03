1637dfcadccba18374682a94624648f6813cbf5e
Amphizale <enforcer.snk@gmail.com>
2019-12-03 07:41:29 +0300

    lets get it started

---
+++ src/main/java/com/enfor/myapp/carcrashmap/CarcrashmapApplication.java
@@ -0,0 +1,13 @@
+package com.enfor.myapp.carcrashmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcrashmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarcrashmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enfor/myapp/carcrashmap/CarcrashmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enfor.myapp.carcrashmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcrashmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
